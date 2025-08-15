5f9325c400ed098a0fcd56204e5f5c32136f0262
Tihomirov Vladislav <7292963@mail.ru>
2025-08-15 13:11:00 +0300

    Add method saveCSVFile in CSV Manager

--- src/main/java/com/_3JLOYBOJLK/App.java
+++ src/main/java/com/_3JLOYBOJLK/App.java
@@ -1,8 +1,57 @@
 package com._3JLOYBOJLK;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Movie movie = new Movie("Поворот не туда",2005,"Майкл Джордан");
-        System.out.println(movie);
+        Scanner sc = new Scanner(System.in);
+        CSVManager csvManager = new CSVManager();
+        List<Movie> movies = csvManager.loadMoviesFromCSV("movies.csv");
+
+        for(Movie movie : movies){
+            System.out.println(movie);
+        }
+
+        //вынести в отдельный метод
+        System.out.println("Input nameOfFile, where you want save: ");
+        int flag = 0;
+        while(flag!=1){
+            String fileForCSVSave = sc.nextLine().trim();
+            int lengthFileName = fileForCSVSave.length();
+           if(fileForCSVSave.toLowerCase().endsWith(".csv")){
+               File file = new File(fileForCSVSave);
+               flag = 1;
+               break;
+           }
+           else if(fileForCSVSave.endsWith(".")){
+               File file = new File(fileForCSVSave+"csv");
+               flag = 1;
+               break;
+           }
+           else if(fileForCSVSave.contains(".")&& !fileForCSVSave.endsWith(".")){
+               File file = new File(fileForCSVSave+".csv");
+               flag = 1;
+               break;
+           }
+           else {
+               File file = new File(fileForCSVSave+".csv");
+               flag = 1;
+               break;
+           }
+
+
+
+
+        }
+
+        csvManager.saveMoviesToCSV(movies, fileForCSVSave);
+        List<Movie> moviesFromCSV = csvManager.loadMoviesFromCSV(fileForCSVSave);
+
+        for(Movie movie: moviesFromCSV){
+            System.out.println(movie);
+        }
     }
 }
